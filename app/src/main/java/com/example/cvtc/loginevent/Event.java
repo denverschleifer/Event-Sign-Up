466a750b21c02fc02da84ebdcafb98e822f71b4c
denverschleifer <dschleifer@student.cvtc.edu>
2017-11-27 16:48:17 -0600

    initial

---
+++ app/src/main/java/com/example/cvtc/loginevent/Event.java
@@ -0,0 +1,13 @@
+package com.example.cvtc.loginevent;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Denver on 11/21/17.
+ */
+
+public class Event extends AppCompatActivity {
+
+
+
+}
