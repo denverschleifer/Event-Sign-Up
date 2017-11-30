7dfabc154a0c2bf40757f7c4a76830c40486a556
denverschleifer <dschleifer@student.cvtc.edu>
2017-11-29 23:55:19 -0600

    added meme functionality but won't load image to imageView

--- app/src/main/java/com/example/cvtc/loginevent/MemeActivity.java
+++ app/src/main/java/com/example/cvtc/loginevent/MemeActivity.java
@@ -1,7 +1,31 @@
 package com.example.cvtc.loginevent;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 
 /**
  * Created by Denver on 11/29/17.
@@ -9,11 +33,175 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MemeActivity extends AppCompatActivity{
 
+    private static final int MY_PERMISSION_REQUEST = 1;
+    private static final int RESULT_LOAD_IMAGE = 2;
+
+
+    Button load, save, share, go;
+
+    TextView textView, textView2;
+
+    EditText editText, editText2;
+
+    ImageView imageView;
+
+    String currentImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meme);
         getSupportActionBar().hide();
 
+        if(ContextCompat.checkSelfPermission(MemeActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MemeActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                ActivityCompat.requestPermissions(MemeActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(MemeActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
+            }
+        } else {
+
+        }
+
+        imageView = findViewById(R.id.imageView);
+
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+
+        imageView = findViewById(R.id.imageView);
+
+        imageView = findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
+
+        go = findViewById(R.id.go);
+
+        load = findViewById(R.id.load);
+        save = findViewById(R.id.save);
+        share = findViewById(R.id.share);
+
+        save.setEnabled(false);
+        share.setEnabled(false);
+
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View content = findViewById(R.id.layout);
+                Bitmap bitmap = getScreenShot(content);
+                currentImage = "meme" + System.currentTimeMillis() + ".png";
+                store(bitmap, currentImage);
+                share.setEnabled(true);
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareImage(currentImage);
+            }
+        });
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+                textView2.setText(editText2.getText().toString());
+
+                editText.setText("");
+                editText2.setText("");
+            }
+        });
+
+    }
+
+    public static Bitmap getScreenShot(View view){
+        view.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+        view.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+
+    public void store(Bitmap bm, String fileName) {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MEME";
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        File file = new File(dirPath, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bm.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Error saving!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void shareImage(String fileName){
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MEME";
+        Uri uri = Uri.fromFile(new File(dirPath, fileName));
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("image/*");
+
+        intent.putExtra(Intent.EXTRA_SUBJECT, "");
+        intent.putExtra(Intent.EXTRA_TEXT, "");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        try{
+            startActivity(Intent.createChooser(intent, "Share via"));
+        }catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "No sharing app found", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String filePathColumn = (MediaStore.Images.Media.DATA);
+            Cursor cursor = getContentResolver().query(selectedImage, new String[]{filePathColumn}, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            save.setEnabled(true);
+            share.setEnabled(false);
+        }
+    }
+
+    public void onRequestPermissionsResult (int requestCode, @NonNull String permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSION_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MemeActivity.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+
+                    }
+                } else {
+                    Toast.makeText(this, "No permission granted!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
     }
 }
